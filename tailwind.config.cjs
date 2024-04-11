ae4ca2fafa13201a162f016dd5f5b84e9cf3a0b4
Kamilla Huhnke <kaan0001@stud.kea.dk>
2024-04-11 11:01:13 +0200

    opstart

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-  };
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        hfarve: 
+      },
+      fontFamily: {},
+      backgroundImage: {
+        mig: "url('/src/assets/images/mig.JPG')",
+      },
+    },
+  },
+};
