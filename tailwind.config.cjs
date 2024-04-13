b77f1b1b5aa0d367bcb7cace4f2c2fe1ccc58f07
Kamilla Huhnke <kaan0001@stud.kea.dk>
2024-04-13 14:46:47 +0200

    d

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,12 +14,21 @@ module.exports = {
         textc: "#2E1915",
       },
       fontFamily: {
-        pops: ["poppins light", "url('https://use.typekit.net/sow1cek.css');"],
+        pops: ["poppins light"],
         pop: [
           "poppins extralight",
-          "url('https://use.typekit.net/sow1cek.css');",
+
+          // "url('https://use.typekit.net/sow1cek.css')",
         ],
       },
+      // fontWeight: {
+      //   pops: "300", // Define font weight 300 as 'light'
+      //   pop: "200", // Define font weight 200 as 'extralight'
+      // },
+      // fontStyle: {
+      //   pops: "normal",
+      //   pop: "normal",
+      // },
       backgroundImage: {
         mig: "url('/src/assets/images/mig.png')",
       },
