130bcba8cc7de8365a4df337fac9fc49b8773889
Kamilla Huhnke <kaan0001@stud.kea.dk>
2024-04-17 09:54:04 +0200

    v

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,8 +14,8 @@ module.exports = {
         textc: "#2E1915",
       },
       fontFamily: {
-        pops: ["poppins light, sans-serif"],
-        pop: ["poppins extralight, sans-serif"],
+        pops: ["poppins light", "sans-serif"],
+        pop: ["poppins extralight", "sans-serif"],
       },
       fontSize: {
         big: "2.75rem",
